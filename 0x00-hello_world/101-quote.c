0ec19dea69c562b3dac0bf545ef4fb24ef422ba7
Kalkidan Simegn Adimasu <kalkidan>
2022-04-07 15:13:59 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 88);
+return (1)
+}
 
