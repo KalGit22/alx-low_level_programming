40f53416e31b883dd0d70bbf09e4c8dbf5dffe57
Kalkidan Simegn Adimasu <kalkidan>
2022-04-07 09:11:55 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#inclde <stdio.h>
 
 /*
  * main is entry point
@@ -8,7 +8,7 @@
 int main(void)
 
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 	    return (0);
 
 }
