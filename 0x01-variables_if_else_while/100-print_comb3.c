fc9794bd26e7b43dd77dd66cde606affe02a6586
Kalkidan Simegn Adimasu <kalkidan>
2022-04-11 13:18:06 -0700

    print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,18 @@
  */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
-{
-for (digit1 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar(digit1 % 10 + '0');
-putchar(digit2 % 10 + '0');
+	int num1, num2;
 
-if (digit1 == 8 && digit2 == 9)
-continue;
-
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-
-return (0);
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
