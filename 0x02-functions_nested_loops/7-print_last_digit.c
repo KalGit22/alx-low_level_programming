8873188090c8dddf7e4dbdcc2c3c99b21964d5e0
Kalkidan Simegn Adimasu <kalkidan>
2022-04-12 15:45:32 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @num: input number
+ * Description: calculates the last digit of a number
+ * Return: last digit of input number num
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
