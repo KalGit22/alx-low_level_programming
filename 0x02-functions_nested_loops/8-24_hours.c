46fdb1d2c078752671d30e5710f7aad71ce348e9
Kalkidan Simegn Adimasu <kalkidan>
2022-04-12 16:46:29 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ * Deskription - display 24 hour
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int minute, hour;
+
+	for (int k = 0; k <= 1440; k++)
+	{
+		hour = k / 60;
+		minute = k - 60 * k;
+		_putchar(hour / 10 + '0');
+		_putchar(hour % 10 + '0');
+		_putchar(':');
+		_putchar(minute / 10 + '0');
+		_putchar(minute % 10 + '0');
+		_putchar('\n');
+	}
+}
