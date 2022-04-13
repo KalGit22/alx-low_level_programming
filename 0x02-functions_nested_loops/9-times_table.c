08a9fc551f386bb72fa5ea41220073f4e533d7a8
Kalkidan Simegn Adimasu <kalkidan>
2022-04-12 19:41:19 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,9 @@ void times_table(void)
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
+
 			if (column == 0)
+			{
 				_putchar('0');
 			}
 			else if (product < 10)
@@ -38,4 +40,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
