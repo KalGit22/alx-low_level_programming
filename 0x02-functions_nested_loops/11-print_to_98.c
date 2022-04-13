9b086cb0d149e1c02a2e937718396755de5e851c
Kalkidan Simegn Adimasu <kalkidan>
2022-04-12 17:44:03 -0700

    print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry point
+ * @n: input number
+ * Description: print up to 98 from a given number
+ * Retrun: Nothing
+ */
+void print_to_98(int n)
+{
+	int index;
+
+	if (n < 98)
+	{
+		for (index = n; index <= 98; index++)
+		{
+			printf("%d, ", index);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (index = n; index >= 98; index--)
+		{
+			printf("%d, ", index);
+		}
+		_putchar('\n');
+	}
+}
