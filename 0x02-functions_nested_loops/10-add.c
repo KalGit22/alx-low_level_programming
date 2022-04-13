20bdecaa57df38335707d567625be4d16d852b62
Kalkidan Simegn Adimasu <kalkidan>
2022-04-12 17:16:26 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * Description: add numbers
+ * @num1: input number 1
+ * @num2: inpput number 2
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
