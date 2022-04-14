268314c02e79294723c5f7edda5a21b934171722
Kalkidan Simegn Adimasu <kalkidan>
2022-04-14 09:22:42 -0700

    100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 
 /**
- * main -  prints the first 98 Fibonacci numbers
- * Return: Always 0
+ * numLength - returns the lenth of string
+ * @num : operand number
+ * Return: number of digits
  */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+/**
+ * main - prints the first 98 fibonaci sequences
+ * Return: 0
+ */
+
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
-
-	for (count = 0; count < 92; count++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000;
+	short int i = 1, initial0s;
 
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
+	while (i <= 98)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half1;
-		if (fib1_half2 + fib2_half2 > 999999999)
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			printf("%i", 0);
+			initial0s--;
 		}
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
+		printf("%lu", f1);
 
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
 	}
-	printf("\n");
 	return (0);
+	}
 }
