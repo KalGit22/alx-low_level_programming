3c1110e7632bc0cd10b6e70a61bb6c8c90c1d887
Kalkidan Simegn Adimasu <kalkidan>
2022-04-14 16:22:00 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper-entry point
+ * @c: print c
+ * Return: 1 for uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
