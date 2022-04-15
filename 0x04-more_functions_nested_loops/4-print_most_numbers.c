8e725c2b414ba2875cfe36d9ce5673f71477f1b0
Kalkidan Simegn Adimasu <kalkidan>
2022-04-14 17:32:20 -0700

    print without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Do not print 2 and 4
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a == 2 || a == 4)
+
+			i++;
+
+		_putchar(a + '0');
+	}
+
+	_putchar('\n');
+}
