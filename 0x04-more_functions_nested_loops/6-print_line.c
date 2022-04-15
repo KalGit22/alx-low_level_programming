0a36ac407587c8cbbce58d77d0f6615188f0cc13
Kalkidan Simegn Adimasu <kalkidan>
2022-04-14 18:21:25 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: variable
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int x = 0;
+
+	while (x < n && n > 0)
+	{
+		_putchar('_');
+		x++;
+	}
+	_putchar('\n');
+}
