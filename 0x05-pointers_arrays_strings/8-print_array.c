835949cefe2d661b0f097fb3a5d3bf268b8dfe5c
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 15:55:12 -0700

    array ptint

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,10 +2,22 @@
 
 /**
  * print_array - prints n elements of an array of integers
- * @n:
- * @*a:
+ * @n: array size
+ * @a:array of n size
  * Return: void
  */
 
 void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+			if (i != 4)
+			{
+				printf(", ");
+			}
+	}
+	printf("\n");
+}
