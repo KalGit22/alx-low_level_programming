82ff085fa8c19ae9457445e1109b94b700438a35
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 16:27:45 -0700

    copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * *_strcpy - 
- * *dest:
- * *src:
- * Return value: the pointer to dest
+ *_strcpy - Copies from source to destinition
+ * @dest: destination buffer
+ * @src: source buffer
+ * Description: Function to copy string
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	while (*src != '\0')
+	{
+		*dest = *src;
+		scr++;
+		dest++;
+	}
+	dest[-1] = '\0';
+	return (dest);
+}
