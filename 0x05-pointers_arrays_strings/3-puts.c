1c7d60a45c945015460a47d1985ed0cfb32209af
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 13:06:43 -0700

    str read

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,9 +2,16 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @*str:
+ * @str: string pointer
  * Return: void
  */
 
 void _puts(char *str)
 {
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
