4b48a08fdad6eec18624c51f6e363dceb95d703d
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 12:25:22 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,18 @@
 
 /**
  * _strlen - returns the length of a string
- * @*s:
+ * @s:character pointer
  * Return: 0
  */
 
 int _strlen(char *s)
 {
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		*s++;
+	}
+	return (count);
+}
