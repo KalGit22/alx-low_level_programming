818b547a2a45f657b4bc101e1bfe2415fa91d8f3
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 14:56:58 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,17 @@
 
 /**
  * print_rev - prints a string, in reverse
- * @*s:
+ * @s: string input
  * Return: void
  */
 
 void print_rev(char *s)
 {
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
