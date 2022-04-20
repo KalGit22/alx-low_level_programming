9dda7b1c88929f2734a5f1ea898282a353f2e815
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 15:28:35 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,9 +2,20 @@
 
 /**
  * rev_string -  reverses a string
- * @*s:
+ * @s: input string pointer
  * Return: void
  */
 
 void rev_string(char *s)
 {
+	char *start = s;
+
+	char end = start + _strlen(start) - 1;
+
+	while (end > start)
+	{
+		char temp = *end;
+		*end-- = *start;
+		*start++ = temp;
+	}
+}
