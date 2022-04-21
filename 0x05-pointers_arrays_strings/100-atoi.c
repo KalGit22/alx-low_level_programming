59ebd3eddfde07c73c9b44d5be90052cb4c89525
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 17:48:23 -0700

    converted string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer 
- * *s:
- * Return: 0
+ * _atoi - convert a string to an integer
+ * @s:input string
+ * Return: converted string
  */
 
 int _atoi(char *s)
 {
+	int x = 1;
+	unsigned int y = 0;
+
+	do {
+		if (*s == '-')
+			x *= -1;
+		else if (*s >= '0' && *s <= '9')
+			y = (y * 10) + (*s - '0');
+		else if (y > 0)
+			break;
+	} while (*s++);
+	return (y * x);
+}
