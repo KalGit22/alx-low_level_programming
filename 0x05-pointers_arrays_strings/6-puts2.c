d7bf28fc8c8af2d6db071d08858ed52ae752ae5e
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 18:23:41 -0700

    char of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,9 +2,21 @@
 
 /**
  * puts2 - prints every other character of a string starting with first one
- * @*str;
+ * @str: string reference
  * Return: void
  */
 
 void puts2(char *str)
 {
+	int m = 0;
+
+	while (str[m] != '\0')
+	{
+		if (m % 2 == 0)
+		{
+			_putchar(str[m]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
