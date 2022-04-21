537dc6c37a97404491bb2254a783a541f4e63f1a
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 18:34:54 -0700

    second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string
- * @*str:
+ * puts_half - print second half of a string
+ * @str: string reference
  * Return: void
  */
 
 void puts_half(char *str)
 {
+	int k;
+
+	for (k = 0; str[k] !- '\0'; k++)
+		;
+	k++;
+	for (k /=2; str[k] != '\0'; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
