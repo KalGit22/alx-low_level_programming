643508146d485580c10332746c9b8f644e963544
Kalkidan Simegn Adimasu <kalkidan>
2022-04-20 18:51:23 -0700

    random

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main -  generates random valid passwords for the program 101-crackme
@@ -6,4 +8,18 @@
  * Return: 0
  */
 
+int main(void)
+{
+	int x;
+	char ch;
 
+	srand(time(NULL));
+	while (x <= 2645)
+	{
+		ch = rand() % 128;
+		x += ch;
+		putchar(ch);
+	}
+	putchar(2772 - x);
+	return (o);
+}
