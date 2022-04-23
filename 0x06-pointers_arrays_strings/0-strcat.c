89418c9d6fc3afcf66ebcf6410069fe2187dde0b
Kalkidan Simegn Adimasu <kalkidan>
2022-04-23 16:46:29 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
-#include "main.h"
+#include <main.h>
 
-/**
+/** 
  * _strcat - concatenates two strings
  * Return: dest
  */
@@ -9,10 +9,10 @@ char* my_strcat(char* dest, const char* src)
 
 {
 	int i;
-
 	for (i = 0; dest[i] != '\0'; ++i);
 	char* ptr = dest + i;
-	while (*src != '\0') {
+	while (*src != '\0') 
+	{
 		*ptr++ = *src++;
 	}
 	*ptr = '\0';
