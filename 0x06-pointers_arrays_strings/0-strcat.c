bb78f2c908dc21e4d8f830d53f0834dc00cb4087
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 14:59:33 -0700

    cont

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,18 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: destnition pointer
- * @src: source pointer
- * Return: dest
+ *_strcat - concatinating function
+ *@dest: deatination pointer
+ *@src: source pointer
+ *Return: concatinated string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int index = 0;
-	int dest_len = 0;
+	int in = 0, len = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[in++])
+		len++;
+	for (in = 0; src[in]; in++)
+		dest[len++] = src[in];
 	return (dest);
 }
