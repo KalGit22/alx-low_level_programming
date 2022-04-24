5f06ccd99545d1bc10e0553bb8104cc554d0a0b6
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 11:57:48 -0700

    str compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,24 @@
 #include "main.h"
 
+/**
+ * _strcmp - function to compare two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: an int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != *s)
+		{
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
 
