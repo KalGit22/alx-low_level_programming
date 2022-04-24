ec9941f6376ae0898f52f086ec22c3dfbfdbdd75
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 14:33:12 -0700

    upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,2 +1,35 @@
 #include "main.h"
 
+/**
+ * cap_string - function to change given string to upper case
+ * @str: input string
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int point = 0;
+
+	while (str[point])
+	{
+		while (!(str[point] >= 'a' && str[point] <= 'z'))
+			point++;
+		if (str[point - 1] == ' ' ||
+				str[point - 1] == '\t' ||
+				str[point - 1] == '\n' ||
+				str[point - 1] == ',' ||
+				str[point - 1] == ';' ||
+				str[point - 1] == '.' ||
+				str[point - 1] == '!' ||
+				str[point - 1] == '?' ||
+				str[point - 1] == '"' ||
+				str[point - 1] == '(' ||
+				str[point - 1] == ')' ||
+				str[point - 1] == '{' ||
+				str[point - 1] == '}' ||
+				point == 0)
+			str[point] -= 32;
+		point++;
+	}
+	return (str);
+
