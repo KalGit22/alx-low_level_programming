27476e05b09e283b4981d9bb08af09789c18c8bf
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 12:32:31 -0700

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,20 @@
 #include "main.h"
 
+/**
+ * string_toupper - string to capital letter
+ * @str: input string
+ * Return: capitalized string
+ */
+char *string_toupper(char *str)
 
+{
+	int pos = 0;
+
+	while (str[pos])
+	{
+		if (str[pos] >= 'a' && str[pos] <= 'z')
+			str[pos] -= 32;
+		str++;
+	}
+	return (str);
+}
