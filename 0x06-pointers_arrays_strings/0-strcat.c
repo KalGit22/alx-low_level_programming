7fb7634b3802e6f9a8f9caac5000ac745b23a6af
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 09:34:16 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,14 @@
  * Return: dest
  */
 
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int index = 0;
+	int dest_len = 0;
 
-
-	while (src[j++])
-	{
-		j++;
-	}
-	for (i = 0; src[i]; i++)
-	{
-		dest[j++] = src[i];
-	}
-	return (str3);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
