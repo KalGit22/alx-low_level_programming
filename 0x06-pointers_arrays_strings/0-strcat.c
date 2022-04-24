18bd51f69de5e4e3c4387831a7eb187713312acc
Kalkidan Simegn Adimasu <kalkidan>
2022-04-23 22:26:50 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,12 @@
 char *_strcat(char *dest, const char *src)
 {
 	int i;
-	char *ptr = dest + i;
 
 	for (i = 0; dest[i] != '\0'; ++i)
 		;
+	char *ptr;
+
+	*ptr = dest + i;
 	while (*src != '\0')
 	{
 		*ptr++ = *src++;
