e6069942182250966ee5f7ea442e094f125d71b7
Kalkidan Simegn Adimasu <kalkidan>
2022-04-23 22:22:13 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,20 @@
 #include <main.h>
 
-/** 
+/**
  * _strcat - concatenates two strings
+ * @dest: destnition pointer
+ * @src: source pointer
  * Return: dest
  */
 
-char* my_strcat(char* dest, const char* src)
-
+char *_strcat(char *dest, const char *src)
 {
 	int i;
-	for (i = 0; dest[i] != '\0'; ++i);
-	char* ptr = dest + i;
-	while (*src != '\0') 
+	char *ptr = dest + i;
+
+	for (i = 0; dest[i] != '\0'; ++i)
+		;
+	while (*src != '\0')
 	{
 		*ptr++ = *src++;
 	}
