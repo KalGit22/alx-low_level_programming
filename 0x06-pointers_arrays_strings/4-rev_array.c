24ab00f172eebb686c0fab95b9f18206fdb8af58
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 12:08:28 -0700

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,20 @@
 #include "main.h"
 
+/**
+ * reverse_array - function to reverse array elements
+ * @a: input array pointer
+ * @n: size
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
 
+	for (i = 0; i < n / 2; i++)
+	{
+		int tmp = *(a + i);
+
+		*(a + i) = *(a + n - i - 1);
+		*(a + n - i - 1) = tmp;
+	}
+}
