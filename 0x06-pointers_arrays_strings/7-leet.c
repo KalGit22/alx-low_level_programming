f45b32826a7f136ab66837da3d8a39539d2b5df0
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 14:48:21 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ *leet - leet function
+ *@str: input
+ *Return: leeted string
+ */
+
+char *leet(char *str)
+{
+	int indx1 = 0, indx2;
+
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+		indx1++;
+	}
+	return (str);
+}
