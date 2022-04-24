a20a3913ea36890d85cc67dcc91d0bc635c9d2f1
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 10:27:28 -0700

    n size concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenate two strings wth n size
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: number of bytes to concatinate
+ * Description: COncatinate two strings with n bytes
+ * Return: a pointer to the concatianted string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	if((dest == NULL) && (src == NULL))
+	int i, j;
 
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
