23e9387107c860c8c532fac9f74ea0cbf02fee6c
Kalkidan Simegn Adimasu <kalkidan>
2022-04-24 10:41:04 -0700

    str copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,22 @@
 #include "main.h"
 
 /**
+ *_strncpy - function to copy string
+ *@dest: destinition pointer
+ *@src: source pointer
+ *@n: size to copy
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
