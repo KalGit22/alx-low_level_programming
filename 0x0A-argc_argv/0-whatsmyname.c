f11923397796fdc91ed2a50203189354d71ebdcb
Kalkidan Simegn Adimasu <kalkidan>
2022-05-02 18:23:48 -0700

    main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: Number of command line arguments
+ * @argv: Name of the Array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+
+}
