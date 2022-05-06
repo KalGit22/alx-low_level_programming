8928a0e534c78e6bf811472fb532884937a1f21c
Kalkidan Simegn Adimasu <kalkidan>
2022-05-06 12:43:16 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -41,10 +41,10 @@ int coinConverter(int i)
 }
 
 /**
- * main - Takes in one argument for minimum coin count
+ * main - Takes in exactly one argument for minimum coin count
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, otherwise 1
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
  */
 
 int main(int argc, char *argv[])
