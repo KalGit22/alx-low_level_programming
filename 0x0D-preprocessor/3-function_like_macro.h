9f48185562e3915650ff56a28b13cd654bbc912c
Kalkidan Simegn Adimasu <kalkidan>
2022-05-09 03:31:58 -0700

    macro for absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+/**
+ * file - header file
+ * @x: imput number
+ * Description: function-like macro ABS(x) that
+ *              computes the absolute value of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
