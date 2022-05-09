4ef2f2627615dd4dc30762bfdd3cf8e091d4937d
Kalkidan Simegn Adimasu <kalkidan>
2022-05-09 08:03:09 -0700

    micro for sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+/**
+ * File - Header file
+ * @x: first number
+ * @y: second number
+ * Description: computes the sum of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
