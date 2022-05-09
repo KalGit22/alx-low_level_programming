e0d3d541704ba7c5e187b84ad31b6f8cde667b89
Kalkidan Simegn Adimasu <kalkidan>
2022-05-09 02:24:32 -0700

    create micro for PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef _PI_H
+#define _PI_H
+
+/**
+ * File - Header file
+ * Description: defines a macro named PI as an abbreviation
+ *              for the token 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
