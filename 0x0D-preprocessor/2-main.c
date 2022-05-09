8a6d569702474416a1e6cd818e9964a8c813df43
Kalkidan Simegn Adimasu <kalkidan>
2022-05-09 02:59:59 -0700

    to compile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: Number of command line arguments
+ * @argv: Name of the array
+ * Descriptin: prints name of the file it was compiled from
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+}
