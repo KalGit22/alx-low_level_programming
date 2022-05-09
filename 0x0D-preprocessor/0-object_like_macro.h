16909bc9402a9ce521bfe12ef5a2b7489286f330
Kalkidan Simegn Adimasu <kalkidan>
2022-05-09 02:09:38 -0700

    create micro name for size

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,11 @@
-#ifndef _SIZE_
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+/**
+ * File - Header file
+ * Description: defines a macro named SIZE abbreviation for the token 1024
+ */
+
 #define SIZE 1024
 
 #endif
