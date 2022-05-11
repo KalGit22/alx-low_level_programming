031250ffbf09f9112e463fdfea91b68103e2a417
Kalkidan Simegn Adimasu <kalkidan>
2022-05-11 14:37:41 -0700

    read

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTION_POINTER_H_
 #define _FUNCTION_POINTER_H_
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
