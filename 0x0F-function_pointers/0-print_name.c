173fb48ea9ceabc54e3e2c27420f1d9ffbd89958
Kalkidan Simegn Adimasu <kalkidan>
2022-05-11 14:32:06 -0700

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,9 @@
  * @name: The name to be printed.
  * @f: A pointer to a function 
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+i	if (name == NULL || f == NULL)
 		return;
 
 	f(name);
