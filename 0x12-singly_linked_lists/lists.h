7465f5a7db159f43e36982331b464b85a72dae35
Kalkidan Simegn Adimasu <kalkidan>
2022-05-19 05:53:18 -0700

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H_
 #define LISTS_H_
 
+/**
+ * Description: header file containing prototypes
+ */
+
 #include <stdlib.h>
 
 /**
@@ -17,6 +21,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
