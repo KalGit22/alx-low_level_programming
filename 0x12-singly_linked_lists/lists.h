011f7ccded15308a045b4844aa476530186d0268
Kalkidan Simegn Adimasu <kalkidan>
2022-05-19 06:05:03 -0700

    print

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H_
 
 /**
- * Description: header file containing prototypes
+ * Desc: header file containing prototypes
  */
 
 #include <stdlib.h>
@@ -13,7 +13,7 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure 
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -27,6 +27,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _putchar(char c);
 
 #endif /* LISTS_H */
