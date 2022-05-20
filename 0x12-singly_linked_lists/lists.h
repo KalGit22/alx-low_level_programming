313988880afdebf2651545bfefed3be934f248aa
Kalkidan Simegn Adimasu <kalkidan>
2022-05-20 09:11:09 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,13 +2,12 @@
 #define LISTS_H
 
 /**
- * Desc: header file containing prototypes
- *
+ * Description - header file containing prototypes and definitions
  */
 
 #include <stdlib.h>
 
-/**
+/*
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
@@ -22,7 +21,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-int _putchar(char c);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
